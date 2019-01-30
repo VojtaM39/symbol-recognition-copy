0b3fc7af2a75f4bf9a05fb4a897e94af30c3bd9b
VojtaM39 <vojtech.maslan1@gmail.com>
2019-01-30 20:58:42 +0100

    Merge branch 'master' of D:AndroidStudioKotlin2SymbolRecognition with conflicts.

--- app/src/main/java/com/example/symbolrecognition/DataClasses.kt
+++ app/src/main/java/com/example/symbolrecognition/DataClasses.kt
@@ -1,3 +1,3 @@
 package com.example.symbolrecognition
 
-data class Line(val point1X : Short?, val point1Y : Short?, val point2X : Short?,val point2Y : Short?)
+data class Line(val angle : Float, val x1 : Short, val y1 : Short, val x2 : Short,val y2 : Short)
