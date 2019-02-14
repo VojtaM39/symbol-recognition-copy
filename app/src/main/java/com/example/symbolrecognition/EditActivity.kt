07390e92d257a17f35e623d261f80785b50379cd
ondrazelinka <ondra.zelinka15@gmail.com>
2019-02-14 12:07:31 +0100

    By Ondra, pridani listview adapteru a uprava vypisu databaze v edit activity

--- app/src/main/java/com/example/symbolrecognition/EditActivity.kt
+++ app/src/main/java/com/example/symbolrecognition/EditActivity.kt
@@ -3,19 +3,32 @@ package com.example.symbolrecognition
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.widget.AdapterView
+import android.widget.ListView
 import android.widget.SimpleCursorAdapter
+import android.widget.Toast
 
 class EditActivity : AppCompatActivity() {
+
+    var listView: ListView? = null
+    var contactList = ArrayList<Contacts>()
+    var adapter: ContactsListViewAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
-        /*
-        val fromColumns = arrayOf(ContactsContract.Data.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER)
-        val toViews = intArrayOf(R.id.display_name, R.id.phone_number)
 
-        val adapter = SimpleCursorAdapter(this, R.layout.activity_edit, cursor, fromColumns, toViews, 0)
-        val listView = getListView()
-        listView.adapter = adapter*/
+        listView = findViewById<ListView>(R.id.listView)
+
+        adapter = ContactsListViewAdapter(this, contactList)
+        (listView as ListView).adapter = adapter
+
+        (listView as ListView).onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, i, l ->
+            Toast.makeText(
+                applicationContext,
+                contactList?.get(i)?.name,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
