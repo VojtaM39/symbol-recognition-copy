a928a5c8d18afc23bb89eaf4a9398fb08a74f3b4
VojtaM39 <vojtech.maslan1@gmail.com>
2019-03-10 21:27:29 +0100

    By Vojta, Uprava MAX_RATIO_DIFF

--- app/src/main/java/com/example/symbolrecognition/Evaulator.kt
+++ app/src/main/java/com/example/symbolrecognition/Evaulator.kt
@@ -21,7 +21,7 @@ class Evaulator {
     private var drewGestureThickness = mutableListOf<Array<Short>>()
 
     //directionsAlgorithm
-    private val MAX_RATIO_DIFF = 0.2f
+    private val MAX_RATIO_DIFF = 0.35f
     //final decision
     private val directionsAlgorithmWeight = 0.35f
     private val thicknessAlgorithmWeight: Float = 1 - directionsAlgorithmWeight //0.65f
