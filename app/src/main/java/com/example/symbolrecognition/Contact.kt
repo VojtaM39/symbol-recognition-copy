8e94838462fd515bc6f473e0d80f4fa4d9636136
Vojta <asd>
2019-03-19 09:52:50 +0100

    By Vojta, Oprava sekani v EditActivity

--- app/src/main/java/com/example/symbolrecognition/Contact.kt
+++ app/src/main/java/com/example/symbolrecognition/Contact.kt
@@ -4,10 +4,14 @@ class Contact
 {
     var gesturesId: Int? = null
     var contactName: String? = null
+    var movesX: MutableList<Array<Short>>? = null
+    var movesY: MutableList<Array<Short>>? = null
 
-    constructor(gesturesId: Int?, contactName: String?)
+    constructor(gesturesId: Int?, contactName: String?, movesX : MutableList<Array<Short>>?, movesY : MutableList<Array<Short>>?)
     {
         this.gesturesId = gesturesId
         this.contactName = contactName
+        this.movesX = movesX
+        this.movesY = movesY
     }
 }
